e26c35a56693a2cf3447c5cf246578e6cef499af
gogbajbobo <grigoriev.maxim@gmail.com>
2023-03-25 08:10:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express')
 const cors = require('cors')
+const db = require('./db')
+
 const app = express()
 const port = 3000
 
 app.use(cors())
 
 app.get('/', (req, res) => {
-    res.send(`Server successfully get request on port ${ port }`)
+
+    const message = `Server successfully get request on port ${ port }`
+    return db.getDatabases()
+        .then(result => res.json({ message, result}))
+        .catch(err => res.json({ message, err }))
+
 })
 
 app.listen(port, () => {
