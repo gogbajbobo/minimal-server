a6a941cb89acd1be1bd271c0e470f41d38cccf2b
gogbajbobo <grigoriev.maxim@gmail.com>
2023-03-26 07:54:12 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,17 +1,20 @@
 const mysql = require('mysql2')
+const config = require('../minimal-server-config.json')
 
 const getDatabases = () => {
 
     const connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        database: 'sesdb'
+        host: config.host || 'localhost',
+        user: config.user || 'root',
+        password: config.password || '',
+        database: config.database || 'sesdb'
     })
 
     return new Promise((res, rej) => {
         connection.query(
             'select database()',
             (err, results) => {
+                console.log('get current database', results)
                 return err ? rej(err) : res(results[0]['database()'])
             })
     })
